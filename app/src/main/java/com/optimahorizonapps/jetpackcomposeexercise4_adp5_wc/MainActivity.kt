80850e7b71c828e9bec0af34d2e25dde5b49600c
Leonardo_13 <leonardo.novak5@gmail.com>
2023-02-17 11:55:07 +0100

    First layout finished (image, name and profession)
    TODO
    - add second layout (phone number, email...)

--- app/src/main/java/com/optimahorizonapps/jetpackcomposeexercise4_adp5_wc/MainActivity.kt
+++ app/src/main/java/com/optimahorizonapps/jetpackcomposeexercise4_adp5_wc/MainActivity.kt
@@ -3,13 +3,20 @@ package com.optimahorizonapps.jetpackcomposeexercise4_adp5_wc
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.optimahorizonapps.jetpackcomposeexercise4_adp5_wc.ui.theme.JetpackComposeExercise4_ADP5_wCTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,12 +36,48 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun BusinessCardScreen() {
+    val image = painterResource(R.drawable.android_pic)
+    Column(
+        Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally)
+    {
+        Column(
+            Modifier.fillMaxSize(1f),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(
+                painter = image,
+                contentDescription = null,
+                modifier = Modifier.size(width = 150.dp, height = 150.dp),
+            )
+            Text(
+                text = "Donald Duck",
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+                modifier = Modifier.padding(top = 12.dp))
+            Text(
+                text = "Professional cartoon actor")
+        }
+        Column(
+            Modifier.fillMaxSize(1f),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            
+            Row {
+                Icon(painter = , contentDescription = )
+            }
+        }
 
+    }
+}
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     JetpackComposeExercise4_ADP5_wCTheme {
-        
+        BusinessCardScreen()
     }
 }
